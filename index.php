3707722930f65098132111ff99ed594051e079d7
heluyao <heluyaosj13@gmail.com>
2013-02-25 15:38:24 +0800

    update

---
+++ index.php
@@ -0,0 +1,17 @@
+﻿<?php
+ $str = 'window.sourceURL = "http://app.ft.com/";alert(window.sourceURL);';
+
+ echo '{"basejs":"'.strProc($str,1).'"}';
+
+function strProc($string,$bIsHtml=false){ 
+if (!$bIsHtml){ 
+	$string=htmlspecialchars($string); 
+} 
+$string=str_replace('/','\/',$string); 
+$string=str_replace('"','\"',$string); 
+
+
+return $string; 
+} 
+
+?>
